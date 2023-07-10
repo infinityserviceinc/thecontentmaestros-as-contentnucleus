eb3b76517b272077ca4693b52fff728552797ac0
Developer <developer@infinityserviceinc.com>
2023-07-10 07:44:12 -0800

    changes obd

--- views/business-plan.php
+++ views/business-plan.php
@@ -593,11 +593,11 @@
                 <p>
                 Our business plan writing company has years of experience completing projects to its credit.
                 </p>
-                <h4 class="sf-txt">
+                <!-- <h4 class="sf-txt">
                   Starting from only 
-                  <!-- <img class="lazy" alt="loading image..." src="assets/images/arrow.webp" style="">  
-                  <span>$450</span>  -->
-                </h4>
+                  <img class="lazy" alt="loading image..." src="assets/images/arrow.webp" style="">  
+                  <span>$450</span> 
+                </h4> -->
                 <div class="combo_btn">
                   <a href="javascript:;" class="btn-theme" onclick="setButtonURL();">Consult an Expert</a>
                   <a href="/order/" class="btn-theme-outline">Get a Quote</a>
